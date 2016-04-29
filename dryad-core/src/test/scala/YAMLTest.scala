2caf7d8a3f2a109c551e756ac278f70dd8a40702
Andy Ai <aiyanbo@growingio.com>
2016-04-29 14:08:30 +0800

    YAML tests

--- dryad-core/src/test/scala/YAMLTest.scala
+++ dryad-core/src/test/scala/YAMLTest.scala
@@ -1,10 +1,12 @@
 import org.scalatest._
 import org.yaml.snakeyaml.Yaml
 
+import scala.collection.mutable
+
 class YAMLTest extends FlatSpec with Matchers {
   "Default test" should "Print something" in {
     val yaml = new Yaml()
-    val x = yaml.load(getClass.getClassLoader.getResourceAsStream("application.yaml"))
+    val x = yaml.loadAs(getClass.getClassLoader.getResourceAsStream("application.yaml"), classOf[mutable.LinkedHashMap[String, String]])
     println(yaml.getName)
     println(x)
   }
