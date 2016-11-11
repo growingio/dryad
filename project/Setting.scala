3a21008a3f2fb84b5e6cf75ede44cc2b312ec790
Andy Ai <aiyanbo@growingio.com>
2016-11-11 16:40:04 +0800

    Enable Scala 2.12 optimizer (l:method)

--- project/Setting.scala
+++ project/Setting.scala
@@ -1,6 +1,7 @@
 import Dependencies.Versions
 import sbt.{AutoPlugin, PluginTrigger}
 import sbt.Keys._
+import sbt._
 
 object Setting extends AutoPlugin {
 
@@ -10,7 +11,17 @@ object Setting extends AutoPlugin {
     version := "1.0.0-SNAPSHOT",
     organization := "io.growing",
     scalaVersion := Versions.scala212,
+    scalacOptions ++= byScalaVersion {
+      case (2, 12) => Seq("-opt:l:method")
+      case (2, 11) => Seq("-Xexperimental")
+    }.value,
     crossScalaVersions := Seq(Versions.scala211, Versions.scala212)
   )
 
+  def byScalaVersion[A](f: PartialFunction[(Int, Int), Seq[A]]): Def.Initialize[Seq[A]] =
+    Def.setting {
+      CrossVersion.partialVersion(scalaVersion.value)
+        .flatMap(f.lift)
+        .getOrElse(Seq.empty)
+    }
 }
